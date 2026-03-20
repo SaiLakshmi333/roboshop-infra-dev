24f8f026f3c6e730ba9f24e220248cf311fecd34
sailakshmi <gunturusai929@gmail.com>
2026-03-20 22:57:22 +0530

    changes

---
+++ 00-vpc/main.tf
@@ -0,0 +1,7 @@
+module "vpc"{
+    #source= "git::https://github.com/SaiLakshmi333/terraform-aws-vpc.git"
+    source= "../../terraform-aws-vpc"
+    project=var.project
+    environment=var.environment
+    is_peering_required="true"
+}
