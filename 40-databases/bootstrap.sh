c4eab4bc49c3621469c9386d8aa2ab793627b364
sailakshmi <gunturusai929@gmail.com>
2026-03-21 18:24:26 +0530

    changes

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -7,6 +7,6 @@ dnf install ansible -y
 cd /home/ec2-user
 git clone https://github.com/SaiLakshmi333/roboshop-ansible-roles-tf.git
 
-cd ansible-roboshop-roles-tf
+cd roboshop-ansible-roles-tf
 git pull
 ansible-playbook -e component=$component -e env=$environment roboshop.yaml
