43a049cfa1787336f25444b54cfe03ffc5c83d11
sailakshmi <gunturusai929@gmail.com>
2026-03-28 19:14:10 +0530

    changes

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z067791029EEJ0FAK30QG"
+}
+
+variable "domain_name" {
+    default = "devopswithsai.online"
+}
